10599f58f94809cf1fd29abb90e7517d737a7ea8
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-09 13:59:53 +0100

    create state for every new Habit

--- src/component/CreateNewHabit.jsx
+++ src/component/CreateNewHabit.jsx
@@ -3,5 +3,9 @@ import { useState } from "react";
 function CreateNewHabit() {
     let [habits, setHabits] = useState([]);
 
+    let [title, setTitle] = useState();
+    let [repeat, setRepeat] = useState();
+    let [priority, setPriority] = useState();
+
     
 }
