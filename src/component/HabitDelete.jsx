806b71d6092a39f29161299ad9b2e6dfd2685353
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-09 15:25:31 +0100

    start on function for delete

--- src/component/HabitDelete.jsx
+++ src/component/HabitDelete.jsx
@@ -1,10 +1,12 @@
+import React from "react"
+
 function HabitDelete (
 
-)
-
-return (
-    <>
-    </>
+    return (
+        <>
+            <button onClick={deleteHabit(i)}>Delete</button>
+        </>
+    )
 )
 
 export default HabitDelete
