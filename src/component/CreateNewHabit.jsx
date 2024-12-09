766d331c398f64d9db5be7f2a98975496996eb93
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-09 14:23:02 +0100

    Created h1

--- src/component/CreateNewHabit.jsx
+++ src/component/CreateNewHabit.jsx
@@ -7,5 +7,19 @@ function CreateNewHabit() {
     let [repeat, setRepeat] = useState();
     let [priority, setPriority] = useState();
 
-    
+    const AddHabit = () => {
+        let NewHabit = {
+            title,
+            repeat,
+            priority,
+        }
+
+        setHabits
+    };
+
+    return (
+        <>
+            <h1>Add new habit</h1>
+        </>
+    )
 }
