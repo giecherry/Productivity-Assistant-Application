221084b9c4d441f1e4d13aef4da1e581638b1df5
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-09 14:40:35 +0100

    Create inputs and dropdown for creating habit

--- src/component/CreateNewHabit.jsx
+++ src/component/CreateNewHabit.jsx
@@ -20,6 +20,18 @@ function CreateNewHabit() {
     return (
         <>
             <h1>Add new habit</h1>
+            <div className="createNewHabit">
+                <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+
+                <input type="number" placeholder="Repeat" value={repeat} onChange={(e) => setRepeat(e.target.value)}/>
+
+                <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+                    <option value="" disabled selected>Select priority</option>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                </select>
+            </div>
         </>
     )
 }
