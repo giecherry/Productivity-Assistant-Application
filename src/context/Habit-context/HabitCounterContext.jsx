6b17520bc2c6b3600186091b88702a6b6b0ea27b
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-10 10:19:32 +0100

    useState

--- src/context/Habit-context/HabitCounterContext.jsx
+++ src/context/Habit-context/HabitCounterContext.jsx
@@ -4,10 +4,13 @@ export const HabitCounterContext = createContext();
 
 export function HabitCounterContextProvider ({children}) {
 
-    const [counter, setCounter] = useState();
+    const [counter, setCounter] = useState(0);
 
     return (
         <>
+            <HabitCounterContext.Provider value={{counter,increment}}>
+                {children}
+            </HabitCounterContext.Provider>
         </>
     )
 }
