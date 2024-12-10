4985d7f3fe6e4e6da7a56656dad8b9f1bc17bc74
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-10 09:26:47 +0100

    Moved HabitDelete component

---
+++ src/components/Habit-components/HabitDelete.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function HabitDelete (
+
+    return (
+        <>
+            <button onClick={deleteHabit(i)}>Delete</button>
+        </>
+    )
+)
+
+export default HabitDelete
