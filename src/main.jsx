740597ee923f8a196ac68178e2798d9125c091b9
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-10 14:59:17 +0100

    Ad DeleteHabitContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { HabitCounterContextProvider } from './context/Habit-context/HabitCounterContext.jsx'
+import { DeleteHabitContextProvider } from './context/Habit-context/DeleteHabitContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <DeleteHabitContextProvider>
     <HabitCounterContextProvider>
       <BrowserRouter>    
         <App />
       </BrowserRouter>
     </HabitCounterContextProvider>
+    </DeleteHabitContextProvider>
   </StrictMode>,
 )
