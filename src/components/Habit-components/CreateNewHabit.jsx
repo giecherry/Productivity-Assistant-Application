39002fbf4065e7ecd0cafc89fb7e4a521ebd5488
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-10 11:42:41 +0100

    map for every habit

--- src/components/Habit-components/CreateNewHabit.jsx
+++ src/components/Habit-components/CreateNewHabit.jsx
@@ -5,11 +5,15 @@ import { useContext } from "react";
 function CreateNewHabit() {
     let [habits, setHabits] = useState([]);
 
-    let [title, setTitle] = useState();
-    let [repeat, setRepeat] = useState();
-    let [priority, setPriority] = useState();
+    let {counter, increment} = useContext(HabitCounterContext)
+
+    let [title, setTitle] = useState('');
+    let [repeat, setRepeat] = useState('');
+    let [priority, setPriority] = useState('');
 
     const AddHabit = () => {
+        /* e.preventDefault(); */
+
         let newHabit = {
             title,
             repeat,
@@ -17,19 +21,23 @@ function CreateNewHabit() {
         }
 
         setHabits([...habits, newHabit])
+        increment();
     };
 
     return (
         <>
             <h1>Add new habit</h1>
+            <p>Count your repetitions {counter}</p>
+            <button onClick={increment}>+</button>
+
             <div className="createNewHabit">
-                <form action={AddHabit}>
+                <form onSubmit={AddHabit}>
                     <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required/>
 
                     <input type="number" placeholder="Repeat" value={repeat} onChange={(e) => setRepeat(e.target.value)} required/>
 
                     <select value={priority} onChange={(e) => setPriority(e.target.value)} required>
-                        <option value="" disabled selected>Select priority</option>
+                        <option value="" disabled>Select priority</option>
                         <option value="High">High</option>
                         <option value="Medium">Medium</option>
                         <option value="Low">Low</option>
