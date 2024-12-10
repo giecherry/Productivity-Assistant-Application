ab12c198693ee31d4041526f37841e214feb9ea7
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-10 09:26:26 +0100

    moved CreateNewHabit-component

---
+++ src/components/Habit-components/CreateNewHabit.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import HabitDelete from "../component/components/HabitDelete"
+
+function CreateNewHabit() {
+    let [habits, setHabits] = useState([]);
+
+    let [title, setTitle] = useState();
+    let [repeat, setRepeat] = useState();
+    let [priority, setPriority] = useState();
+
+    const AddHabit = () => {
+        let NewHabit = {
+            title,
+            repeat,
+            priority,
+        }
+
+        setHabits
+    };
+
+    return (
+        <>
+            <h1>Add new habit</h1>
+            <div className="createNewHabit">
+                <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+
+                <input type="number" placeholder="Repeat" value={repeat} onChange={(e) => setRepeat(e.target.value)}/>
+
+                <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+                    <option value="" disabled selected>Select priority</option>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                </select>
+
+                <button onClick={AddHabit}>Save habit</button>
+            </div>
+        </>
+    )
+}
+
+export default CreateNewHabit
