1eb67534069c5d18a57d312c313fa2deb4dfc19d
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-10 11:23:07 +0100

    import HabitCounterContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { HabitCounterContextProvider } from './context/Habit-context/HabitCounterContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>    
-      <App />
-    </BrowserRouter>
+    <HabitCounterContextProvider>
+      <BrowserRouter>    
+        <App />
+      </BrowserRouter>
+    </HabitCounterContextProvider>
   </StrictMode>,
 )
