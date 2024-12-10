0ec091ce68dd8829b3f986de164d9df7333da33b
asia123456789 <asiia92@hotmail.com>
2024-12-10 12:21:29 +0000

    Tog bort =id från min todo för att allt skulle fungera

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import TodosAndActivities from './pages/TodosAndActivities'
-import Habits from './pages/Habits'
-import EventCalendar from './pages/EventCalendar'
-import LogIn from './pages/LogIn'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TodosAndActivities from "./pages/TodosAndActivities";
+import Habits from "./pages/Habits";
+import EventCalendar from "./pages/EventCalendar";
+import LogIn from "./pages/LogIn";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LogIn/>} />
+        <Route path="/" element={<LogIn />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/todo=:id" element={<TodosAndActivities />} />
+        <Route path="/todo" element={<TodosAndActivities />} />
         <Route path="/habits" element={<Habits />} />
         <Route path="/calendar" element={<EventCalendar />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
