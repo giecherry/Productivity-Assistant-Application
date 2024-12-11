03c2fbf617a83aa8f5c5a1a1fd34da53e85e06c6
Angie C <a.cajusol98@gmail.com>
2024-12-11 10:10:11 +0100

    new event small changes

--- src/components/EventCalendar-components/NewEvent.jsx
+++ src/components/EventCalendar-components/NewEvent.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { EventContext } from "./EventContext";
 import NewEventCSS from './NewEvent.module.css'
 
@@ -7,12 +7,19 @@ const NewEvent = () => {
 
     const {events} = useContext(EventContext)
 
+    const [eventTitle, setEventTitle] = useState("")
+    const [eventDate, setEventDate] = useState(null)
+    const [eventStartTime, setEventStartTime] = useState(null)
+    const [eventEndTime, setEventEndTime] = useState(null)
+
+
     const {addEvent} = useContext(EventContext)
     return (
         <>
             <div className={NewEventCSS.newEventContainer}>
                 <h1>New Event</h1>
-                <input className={NewEventCSS.input} type="text" placeholder="Event title" />
+                <input className={NewEventCSS.input} type="text" placeholder="Event title" onChange={(e)=> setEventTitle(e.target.value)}/>
+                {console.log(eventTitle)}
                 <input className={NewEventCSS.input} type="date" placeholder="Event date" />
                 <input className={NewEventCSS.input} type="time" placeholder="Start time" />
                 <input className={NewEventCSS.input} type="time" placeholder="End time" />
