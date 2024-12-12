4f4b0bcb936f7210f41ffbdfb3ec55b9b67405bc
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-12 09:19:17 +0100

    create HabitReduceContext

---
+++ src/context/Habit-context/HabitReduceContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+export const HabitReduceContext = createContext();
+
+export function HabitReduceContextProvider ({children}) {
+
+    const [reduceCounter, setReduceCounter] = useState(1);
+
+    const reduce = () => {
+        setReduceCounter(reduceCounter-1);
+    }
+
+    return (
+        <>
+            <HabitReduceContext.Provider value={{reduceCounter,reduce}}>
+                {children}
+            </HabitReduceContext.Provider>
+        </>
+    )
+}
