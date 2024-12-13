c39ecb56063cebadcf87f284e08f44de0605129d
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-13 08:09:05 +0100

    LocalStorage

--- src/components/Habit-components/CreateNewHabit.jsx
+++ src/components/Habit-components/CreateNewHabit.jsx
@@ -1,28 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { HabitCounterContext } from "../../context/Habit-context/HabitCounterContext";
 import { HabitReduceContext } from '../../context/Habit-context/HabitReduceContext.jsx';
 import { DeleteHabitContext } from "../../context/Habit-context/DeleteHabitContext.jsx";
-import { useContext } from "react";
 
 function CreateNewHabit() {
-    let [habits, setHabits] = useState([]);
+
+    const [habits, setHabits] = useState(JSON.parse(sessionStorage.getItem("Data")) || []);
 
     let {counter, increment} = useContext(HabitCounterContext)
     let {reduceCounter, reduce} = useContext(HabitReduceContext)
-    let {deleteHabit, reset} = useContext(DeleteHabitContext)
+    let {reset} = useContext(DeleteHabitContext)
 
     let [title, setTitle] = useState('');
+    let [description, setDescription] = useState('');
     let [repeat, setRepeat] = useState(0);
     let [priority, setPriority] = useState('');
 
+    useEffect(() => {
+        console.log("Save habit in sessionStorage")
+
+        sessionStorage.setItem("Data", JSON.stringify(habits))
+    }, [habits])
+
     const AddHabit = (e) => {
         e.preventDefault();
+
         let newHabit = {
             id:Date.now(),
             title,
+            description,
             repeat,
             priority,
-        }
+        };
 
         let updatedHabits = [...habits, newHabit]
         setHabits(updatedHabits)
@@ -32,11 +41,13 @@ function CreateNewHabit() {
 
     return (
         <>
-            <h1>Add new habit</h1>
+            <h4 className="habitHFour">Add new habit</h4>
 
             <div className="createHabit">
                 <form onSubmit={AddHabit}>
                     <input className="HabitTitle" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required/>
+
+                    <input className="HabitDescription" type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}/>
 
                     <input className="HabitRepeat" type="number" placeholder="Repeat" value={repeat} onChange={(e) => setRepeat(e.target.value)} required/>
 
@@ -52,7 +63,7 @@ function CreateNewHabit() {
 
                 <ul>
                     {habits.map((habit, i) => (
-                    <li key={i}>
+                    <li className="habitCard" key={habit.id}>
                         <h3>Habit: {habit.title}</h3>
                         <p>Repeat: {habit.repeat}</p>
                         <p>Priority: {habit.priority}</p>
