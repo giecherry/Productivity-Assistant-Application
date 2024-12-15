71450728a6c6c72fa4f7d755e5a7a2adc38f73db
Angie C <a.cajusol98@gmail.com>
2024-12-15 21:49:27 +0100

    link to individual pages

--- src/components/HomePage-components/CalendarOverview.jsx
+++ src/components/HomePage-components/CalendarOverview.jsx
@@ -1,10 +1,12 @@
 import HomePageCSS from "./HomePage.module.css"
+import { Link } from "react-router-dom";
 
 const CalendarOverview = () => {
     return (
         <>
             <div className={HomePageCSS.calendarContainer}>
                 <h1>Calendar Overview</h1>
+                <Link to="/calendar"><button>Show more</button></Link>
             </div>
         </>
     )
--- src/components/HomePage-components/TodosOverview.jsx
+++ src/components/HomePage-components/TodosOverview.jsx
@@ -1,10 +1,13 @@
 import HomePageCSS from "./HomePage.module.css"
+import { Link } from "react-router-dom";
+
 
 const TodosOverview = () => {
     return (
         <>
         <div className={HomePageCSS.todosContainer}>
             <h1>Todos Overview</h1>
+            <Link to="/todos"><button>Show more</button></Link>
         </div>
             
         </>
