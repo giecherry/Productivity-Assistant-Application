db016edbc69d0bbf89bfe75dae5f209508aa561a
Angie C <a.cajusol98@gmail.com>
2024-12-15 17:23:47 +0100

    added GreetingAndQuote to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
+import GreetingAndQuote from "../components/HomePage-components/Greeting-quote"
 
 const HomePage = () => {
 
-    let [random, setRandom] = useState([]);
+/*     let [random, setRandom] = useState([]);
 
     useEffect (() => {getRandom();},[])
     const getRandom = async () => {
@@ -13,11 +14,12 @@ const HomePage = () => {
     if(location.state){
         setRandom(location.state);
     }
-
+ */
     return (
         <>
-            <h1>hej</h1>
-            <h2>Hello xxx here is a fun quote for you {random}</h2>
+            <GreetingAndQuote/>
+            {/* <h1>hej</h1>
+            <h2>Hello xxx here is a fun quote for you {random}</h2> */}
         </>
     )   
 }
