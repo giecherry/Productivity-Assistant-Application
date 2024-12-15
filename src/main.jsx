caa181b12bd6c84e74c08e39e83df6cf64e391c5
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-15 12:42:08 +0100

    delete import for HabitReduceContext

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,18 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { HabitCounterContextProvider } from './context/Habit-context/HabitCounterContext.jsx'
 import { DeleteHabitContextProvider } from './context/Habit-context/DeleteHabitContext.jsx'
-import { HabitReduceContextProvider } from './context/Habit-context/HabitReduceContext.jsx'
 import { HabitFiltSortContextProvider } from './context/Habit-context/HabitFiltSortContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HabitFiltSortContextProvider>
-      <HabitReduceContextProvider>
-        <DeleteHabitContextProvider>
-          <HabitCounterContextProvider>
-            <BrowserRouter>    
-              <App />
-            </BrowserRouter>
-          </HabitCounterContextProvider>
-        </DeleteHabitContextProvider>
-      </HabitReduceContextProvider>
+      <DeleteHabitContextProvider>
+        <HabitCounterContextProvider>
+          <BrowserRouter>    
+            <App />
+          </BrowserRouter>
+        </HabitCounterContextProvider>
+      </DeleteHabitContextProvider>
     </HabitFiltSortContextProvider>
   </StrictMode>,
 )
