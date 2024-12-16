fbec1db72d0c79387e4efff9a3f33e5456f8cf78
Angie C <a.cajusol98@gmail.com>
2024-12-16 20:20:05 +0100

    delete test events

--- src/components/EventCalendar-components/Calendar.jsx
+++ src/components/EventCalendar-components/Calendar.jsx
@@ -16,17 +16,6 @@ const Calendar = () => {
     const handlePrevMonth = () => setCurrentMonth(subMonths(currentMonth, 1));
     const handleNextMonth = () => setCurrentMonth(addMonths(currentMonth, 1));
 
-    //*Test events-------------------
-    const testEvents=[
-            { id: 1, date: new Date(2024, 11, 5, 10, 45), title: "Sprint Planning", time: "10:45-11:00" },
-            { id: 2, date: new Date(2024, 11, 6, 10, 45), title: "Performance review", time: "10:45-12:00" },
-            { id: 3, date: new Date(2024, 10, 7, 10, 45), title: "Bi-weekly sync w/Alyne", time: "10:45-12:00" },
-            { id: 4, date: new Date(2024, 10, 8, 10, 45), title: "Mid-week check-in", time: "10:45-11:00" },
-            { id: 5, date: new Date(2024, 11, 9, 10, 45), title: "Retro", time: "10:45-11:00" },
-            { id: 6, date: new Date(2024, 11, 10, 10, 45), title: "Studio meetup", time: "10:45-11:00" },
-            { id: 7, date: new Date(2024, 11, 11, 10, 45), title: "Marketing Monthly", time: "10:45-11:00" },
-            { id: 8, date: new Date(2024, 11, 12, 10, 45), title: "Weekly Planning", time: "10:45-11:00" },
-    ];
 
     return (
         <>
