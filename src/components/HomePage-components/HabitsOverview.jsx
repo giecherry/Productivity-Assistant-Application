74b64b75b51644eff5a66cba6c8a4b93bb0e523c
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-17 16:43:59 +0100

    start for sorti habits

--- src/components/HomePage-components/HabitsOverview.jsx
+++ src/components/HomePage-components/HabitsOverview.jsx
@@ -1,12 +1,29 @@
+import { useContext } from "react";
 import HomePageCSS from "./HomePage.module.css"
 import { Link } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 const HabitsOverview = () => {
+
+    const {inUser} = useContext(UserContext);
+
+    const highestCount = habitsContainer
+        .sort((a, b) => high count(a.count) - high count(b.count))
+        .slice(0, 3);
 
     return (
         <>
             <div className={HomePageCSS.habitsContainer}>
                 <h1 className="HabitHOne">Habits</h1>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
                 <Link to="/habits"><button>Show more</button></Link>
             </div>
         </>
