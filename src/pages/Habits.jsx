3a296beeb786fa7f621608c885c57805db0c2fcf
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-17 15:22:06 +0100

    Array

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -1,12 +1,28 @@
 import CreateNewHabit from "../components/Habit-components/CreateNewHabit"
 
+const [ifNoOther] = useState([
+    {
+        title: "Clean",
+        priority: "High",
+    },
+    {
+        title: "Train",
+        priority: "Medium",
+    },
+    {
+        title: "Paint",
+        priority: "Low",
+    }
+]);
+
 const Habits = () => {
     return (
         <>
             <div className="HabitHome">
                 <h1 className="HabitHOne">Habits</h1>
+
+                <h5>Go to your habits<Link to="/createNewHabit" element= {<CreateNewHabit />} >Sign up!</Link></h5>
                 
-                <CreateNewHabit />
             </div>
         </>
     )
