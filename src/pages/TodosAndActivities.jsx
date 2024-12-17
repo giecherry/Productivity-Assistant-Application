c37fd265e45b4ac01c81951981557630823abc45
Asia123456789 <asiia92@hotmail.com>
2024-12-17 15:47:00 +0000

    Skapade vareabels, useStates och onCanges

--- src/pages/TodosAndActivities.jsx
+++ src/pages/TodosAndActivities.jsx
@@ -4,11 +4,16 @@ import { useState } from "react";
       https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label*/
 function TodosAndActivities() {
   const [todos, setTodos] = useState([]);
-  const [currentTodoText, setCurrentTodoText] = useState("");
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoCategory, setTodoCategory] = useState("");
+  const [todoDescription, setTodoDescription] = useState("");
+  const [todoStatus, setTodoStatus] = useState("");
+  const [todoEsTime, setTodoEsTime] = useState("");
+  const [todoDeadline, setTodoDeadline] = useState("");
 
   function addTodo(e) {
     e.preventDefault();
-    console.log(currentTodoText);
+    console.log(todoTitle);
   }
   return (
     <div className="App">
@@ -26,14 +31,18 @@ function TodosAndActivities() {
       <form onSubmit={() => addTodo()}>
         <input
           type="text"
-          value={currentTodoText}
-          onChange={(e) => setCurrentTodoText(e.target.value)}
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
           placeholder="Enter todo here"
         />
         <br />
         <br />
         <label htmlFor="kategori">Category: </label>
-        <select name="kategori" id="kategori">
+        <select
+          name="kategori"
+          id="kategori"
+          onChange={(e) => setTodoCategory(e.target.value)}
+        >
           <option value="1">Health</option>
           <option value="2">Housekeeping</option>
           <option value="3">Job related</option>
@@ -42,18 +51,39 @@ function TodosAndActivities() {
         <br />
         <br />
         <label htmlFor="beskrivning">Description: </label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => setTodoDescription(e.target.value)}
+        />
         <br />
         <br />
         <label htmlFor="Checkbox">Status - Completed</label>
-        <input type="checkbox" />
+        <select
+          name="status"
+          id="status"
+          onChange={(e) => setTodoStatus(e.target.value)}
+        >
+          <option value="1">Completed</option>
+          <option value="1">Uncompleted</option>
+        </select>
         <br />
         <h4>Tidsestimat - Hur lång tid ärendet tar att utföra.</h4>
         <label htmlFor="tidsestimat">Estimated time: </label>
-        <input placeholder="min" type="number" name="" min={1} />
+        <input
+          placeholder="min"
+          type="number"
+          name=""
+          min={1}
+          onChange={(e) => setTodoEsTime(e.target.value)}
+        />
         <h4>Deadline - Datum som det senast ska vara utfört.</h4>
         <label htmlFor="deadline">Deadline: </label>
-        <input type="date" name="" id="" />
+        <input
+          type="date"
+          name=""
+          id=""
+          onChange={(e) => setTodoDeadline(e.target.value)}
+        />
         <br />
         <br />
         <br />
