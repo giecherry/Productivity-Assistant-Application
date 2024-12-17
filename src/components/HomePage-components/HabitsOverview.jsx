a85a49e15bbacbf35621b078855fcdd5ea0b78e3
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-17 16:11:44 +0100

    New h1

--- src/components/HomePage-components/HabitsOverview.jsx
+++ src/components/HomePage-components/HabitsOverview.jsx
@@ -2,10 +2,11 @@ import HomePageCSS from "./HomePage.module.css"
 import { Link } from "react-router-dom";
 
 const HabitsOverview = () => {
+
     return (
         <>
             <div className={HomePageCSS.habitsContainer}>
-                <h1>Habits Overview</h1>
+                <h1 className="HabitHOne">Habits</h1>
                 <Link to="/habits"><button>Show more</button></Link>
             </div>
         </>
