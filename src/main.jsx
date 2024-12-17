fbf3e246ae4a726aa05937f9f479e245b5999161
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-17 16:43:12 +0100

    delete and import context

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { UserContextProvider } from './components/UserContext.jsx'
 import { HabitCounterContextProvider } from './context/Habit-context/HabitCounterContext.jsx'
 import { DeleteHabitContextProvider } from './context/Habit-context/DeleteHabitContext.jsx'
 import { HabitFiltSortContextProvider } from './context/Habit-context/HabitFiltSortContext.jsx'
@@ -12,20 +13,13 @@ createRoot(document.getElementById('root')).render(
     <HabitFiltSortContextProvider>
       <DeleteHabitContextProvider>
         <HabitCounterContextProvider>
-          <BrowserRouter>    
-            <App />
-          </BrowserRouter>
+          <UserContextProvider>
+            <BrowserRouter>    
+              <App />
+            </BrowserRouter>
+          </UserContextProvider>
         </HabitCounterContextProvider>
       </DeleteHabitContextProvider>
     </HabitFiltSortContextProvider>
-import { UserContextProvider } from './components/UserContext.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <UserContextProvider>
-      <BrowserRouter>    
-        <App />
-      </BrowserRouter>
-    </UserContextProvider>
   </StrictMode>,
 )
