4598f4403deee08b5203fe324403ad5b72e474c6
Asia123456789 <asiia92@hotmail.com>
2024-12-17 14:48:26 +0000

    Flyttade mitt arbete från sandbox till github

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import TodosAndActivities from "./pages/TodosAndActivities";
-import Habits from "./pages/Habits";
-import EventCalendar from "./pages/EventCalendar";
-import LogIn from "./pages/LogIn";
-=======
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
@@ -16,18 +7,13 @@ import EventCalendar from './pages/EventCalendar'
 import LogIn from './pages/LogIn'
 import SignUp from './pages/SignUp'
 
->>>>>>> origin/main
 
 function App() {
   return (
     <>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<LogIn />} />
-=======
         <Route path="/" element={<LogIn/>} />
         <Route path="/signup" element={<SignUp/>} />
->>>>>>> origin/main
         <Route path="/home" element={<HomePage />} />
         <Route path="/todo" element={<TodosAndActivities />} />
         <Route path="/habits" element={<Habits />} />
