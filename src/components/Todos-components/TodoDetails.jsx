8498ed3d61e60c418c056f02e87efdd0662ba0d5
Asia123456789 <asiia92@hotmail.com>
2024-12-18 22:23:36 +0000

    Skapat Import, function grunden, export

---
+++ src/components/Todos-components/TodoDetails.jsx
@@ -0,0 +1,9 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { useContext } from 'react'
+import { TodoContext } from '../Todos-components/TodosContext';
+
+function TodoDetails() {
+    
+}
+
+export default TodoDetails;
