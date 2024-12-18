d6ee04a49e3f9cdbeca7f9d1fdcfdbeadddd47b6
Asia123456789 <asiia92@hotmail.com>
2024-12-18 22:36:15 +0000

    Variabels

--- src/components/Todos-components/TodoDetails.jsx
+++ src/components/Todos-components/TodoDetails.jsx
@@ -3,7 +3,11 @@ import { useContext } from 'react'
 import { TodoContext } from '../Todos-components/TodosContext';
 
 function TodoDetails() {
-    
+    //Vad kommer vi behöva för variablar
+    //Denna hämtar id från URLen
+    const { id } = useParams(); 
+    const navigate = useNavigate();
+    const { todos, updateTodo, deliteTodo } = useContext (TodoContext);
 }
 
 export default TodoDetails;
