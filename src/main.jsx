80e686db8c304c54f8797122a7fe1a53537bad64
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-18 18:42:43 +0100

    delete space

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import { EventContextProvider } from "./components/EventCalendar-components/Even
 import { UserContextProvider } from './components/UserContext.jsx'
 import { HabitCounterContextProvider } from './context/Habit-context/HabitCounterContext.jsx'
 import { HabitFiltSortContextProvider } from './context/Habit-context/HabitFiltSortContext.jsx'
-
 import {TodoContextProvider} from './components/Todos-components/TodosContext.jsx'
 
 createRoot(document.getElementById('root')).render(
