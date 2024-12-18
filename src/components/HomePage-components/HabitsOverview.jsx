33efe6f3797eef93f236022da05116478fe42ec4
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-18 10:20:05 +0100

    div for overview

--- src/components/HomePage-components/HabitsOverview.jsx
+++ src/components/HomePage-components/HabitsOverview.jsx
@@ -3,27 +3,40 @@ import HomePageCSS from "./HomePage.module.css"
 import { Link } from "react-router-dom";
 import { UserContext } from "../UserContext";
 
-const HabitsOverview = () => {
+const HabitsOverview = ({habits}) => {
 
     const {inUser} = useContext(UserContext);
 
-    const highestCount = habitsContainer
-        .sort((a, b) => high count(a.count) - high count(b.count))
+    const highestCount = habits
+        .filter(habit => habit.owner === inUser.userName)
+        .sort((a, b) => b.repeat - a.repeat)
         .slice(0, 3);
 
     return (
         <>
             <div className={HomePageCSS.habitsContainer}>
                 <h1 className="HabitHOne">Habits</h1>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-                <div>
-                    
-                </div>
+                {highestCount.map((habit) => (
+                    <div key={habit.id}>
+                        <div>
+                            <div>
+                                <h4>{habit.title}</h4>
+                                <h3>{habit.repeat}</h3>
+                                <h3>{habit.priority}</h3>
+                            </div>
+                            <div>
+                                <h4>{habit.title}</h4>
+                                <h3>{habit.repeat}</h3>
+                                <h3>{habit.priority}</h3>
+                            </div>
+                            <div>
+                                <h4>{habit.title}</h4>
+                                <h3>{habit.repeat}</h3>
+                                <h3>{habit.priority}</h3>
+                            </div>
+                        </div>
+                    </div>
+                ))}
                 <Link to="/habits"><button>Show more</button></Link>
             </div>
         </>
