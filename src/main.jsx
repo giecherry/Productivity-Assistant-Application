83266fd72b34085364c6d46d5560963bc1dc0b1e
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-18 12:59:24 +0100

    delete import for DeleteHabit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { EventContextProvider } from "./components/EventCalendar-components/EventContext";
 import { UserContextProvider } from './components/UserContext.jsx'
 import { HabitCounterContextProvider } from './context/Habit-context/HabitCounterContext.jsx'
-import { DeleteHabitContextProvider } from './context/Habit-context/DeleteHabitContext.jsx'
 import { HabitFiltSortContextProvider } from './context/Habit-context/HabitFiltSortContext.jsx'
 
 import {TodoContextProvider} from './components/Todos-components/TodosContext.jsx'
@@ -15,18 +14,16 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContextProvider>
       <HabitFiltSortContextProvider>
-        <DeleteHabitContextProvider>
-          <HabitCounterContextProvider>
-            <EventContextProvider>
-              <TodoContextProvider>
-                <BrowserRouter>    
-                  <App />
-                </BrowserRouter>
-              </TodoContextProvider>
-            </EventContextProvider>
-          </HabitCounterContextProvider>
-        </DeleteHabitContextProvider>
+        <HabitCounterContextProvider>
+          <EventContextProvider>
+            <TodoContextProvider>
+              <BrowserRouter>    
+                <App />
+              </BrowserRouter>
+            </TodoContextProvider>
+          </EventContextProvider>
+        </HabitCounterContextProvider>
       </HabitFiltSortContextProvider>
-      </UserContextProvider>
+    </UserContextProvider>
   </StrictMode>,
 )
