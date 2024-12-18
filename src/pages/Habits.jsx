b8102b7c7c4e591e21024284a893e831d1f60433
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-18 18:45:00 +0100

    moved function here

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState, useContext } from "react";
 import CreateNewHabit from "../components/Habit-components/CreateNewHabit"
+import { HabitCounterContext } from "/src/context/Habit-context/HabitCounterContext";
+import HabitFiltSort from "../components/Habit-components/HabitFiltSort";
 
 const Habits = () => {
+    
+    let {increment, reduce, zero, reset, habits, AddHabit} = useContext(HabitCounterContext)
+
+    useEffect(() => {
+            console.log("Save habit in localStorage")
+            localStorage.setItem("habit", JSON.stringify(habits))
+        }, [habits])
+
     return (
         <>
             <div className="HabitHome">
                 <h1 className="HabitHOne">Habits</h1>
+                <CreateNewHabit />
+                <HabitFiltSort />   
 
-                <h5>Go to your habits<Link to="/createNewHabit" element= {<CreateNewHabit />} >Sign up!</Link></h5>
-                
+                <div>  
+                    <ul>
+                        {habits.map((habits) => (
+                            <li key={habits.id}>        
+                                <h3>Habit: {habits.title}</h3>
+                                <p>Repeat: {habits.repeat}</p>
+                                <p>Priority: {habits.priority}</p>
+                                <p>Count your repetitions {habits.counter}</p>
+                                <button className="HabitButton" onClick={() => increment(habits.id, habits, setHabits)}>+</button>
+                                <button className="HabitButton" onClick={() => reduce(habits.id, habits, setHabits)}>-</button>
+                                <button className="HabitButton" onClick={() => zero(habits.id, habits, setHabits)}>Reset counter</button>
+                                <button className="HabitButton"onClick={() => reset(habits.id, habits, setHabits)}>Delete</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </>
     )
