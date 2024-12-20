ef7c7c855ad6f385bbe35a3ee2debb1124595458
Angie C <a.cajusol98@gmail.com>
2024-12-20 13:25:44 +0100

    css calendar

--- src/components/EventCalendar-components/Calendar.jsx
+++ src/components/EventCalendar-components/Calendar.jsx
@@ -35,7 +35,7 @@ const Calendar = () => {
 
     return (
         <>
-        <div>
+        <div className={CalendarCSS.CaledarBody}>
             <h1>{format(currentMonth, "yyyy")}</h1>
             <div className={CalendarCSS.monthSelector}>
                 <button className={CalendarCSS.prevMonthButton} onClick={handlePrevMonth}>&lt;</button>
