b5cf4f9a60eeb4918a4a3591769c9a903f0becd7
Sandra Linnea <sandra.b93@hotmail.com>
2024-12-20 14:10:40 +0100

    import module

--- src/components/Habit-components/HabitFiltSort.jsx
+++ src/components/Habit-components/HabitFiltSort.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { HabitCounterContext } from "../../context/Habit-context/HabitCounterContext";
+import HabitCSS from "./Habit.module.css";
 
 function HabitFiltSort () {
 
@@ -32,11 +33,11 @@ function HabitFiltSort () {
 
     return (
         <>
-            <div className="SortAndFilt">
+            <div className={HabitCSS.FiltAndSort}>
                 <h5>Filter and sort your habits</h5>
                 <div className="FiltSort">
                     <label htmlFor="FilterPriority">Filter by priority</label>
-                    <select className="FilterPriority" id="FilterPriority" value={filterHabits} onChange={(e) => setFilterHabits(e.target.value)}>
+                    <select className={HabitCSS.FilterPriority} id="FilterPriority" value={filterHabits} onChange={(e) => setFilterHabits(e.target.value)}>
                         <option value="" disabled>Filter habits</option>
                         <option value="All">All</option>
                         <option value="High">High</option>
@@ -47,13 +48,13 @@ function HabitFiltSort () {
                 
                 <div className="SortDiv">
                 <label htmlFor="SortPriority">Sort by priority</label>
-                    <select className="SortPriority" id="SortPriority" onChange={(e) => setSortHabits('priority')}>
+                    <select className={HabitCSS.SortPriority} id="SortPriority" onChange={(e) => setSortHabits('priority')}>
                         <option value="" disabled>Sort habits by priority</option>
                         <option value="asc">Low to high</option>
                         <option value="desc">High to low</option>
                     </select>
                     <label htmlFor="SortRepear">Sort by repeat</label>
-                    <select className="SortRepeat" id="SortRepear" onChange={(e) => setSortHabits('repeat')}>
+                    <select className={HabitCSS.SortRepeat} id="SortRepear" onChange={(e) => setSortHabits('repeat')}>
                         <option value="" disabled>Sort habits by repeat</option>
                         <option value="asc">Lowest to highest</option>
                         <option value="desc">Highest to lowest</option>
